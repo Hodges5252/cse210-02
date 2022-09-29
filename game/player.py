9a1cef7c53d199c1b9aaf3328308d699d3a3f94e
hodgese <98342515+Hodges5252@users.noreply.github.com>
2022-09-29 15:28:12 +0000

    Post meeting changes, program compiles.

--- game/player.py
+++ game/player.py
@@ -3,14 +3,14 @@ from game.card import Card
 
 class Player:
     def __init__(self):
+        self.current_card = 0
         self.old_card = 0
         self.is_playing = True
         self.guess = True
         self.score = 300
-        self.card = []
-
         card = Card()
-        self.card.append(card)
+        self.card = card
+
 
     def start_game(self):
         while self.is_playing:
@@ -25,35 +25,34 @@ class Player:
         if not self.is_playing:
             return 
 
-        if(self.guess == (self.card > self.old_card)):
+        if(self.guess == (self.current_card > self.old_card)):
             self.score += 100
         else:
             self.score -= 75
-        self.old_card = self.card
+        self.old_card = self.current_card
 
     def do_outputs(self):
         if not self.is_playing:
             return
-          
+
+##This part below has some issues
         card = self.card
         card.pick()
-        card.value = self.card
+        self.current_card = card
 
-        print(f"The card is {self.card}")
+        print(f"The card is {self.current_card}")
         high_low = input("Higher or lower? [h/l] ")
         if (high_low == "h"):
           self.guess = True
         else: 
           self.guess == False
 
+        self.do_updates()
+
         self.is_playing = (self.score > 0)
         if not self.is_playing:
-            print(f"You rolled:")
-            print("No 1's or 5's! You lose!")
-            print(f"Your final score was: {self.total_score}\n")
+            print("Your score is 0! You lose!")
             return
         
-        print(f"You rolled:")
-        print(f"Your score is: {self.total_score}\n")
-        
-        self.score = 0
+        print(f"Next card was: {self.card}")
+        print(f"Your score is: {self.score}\n")
